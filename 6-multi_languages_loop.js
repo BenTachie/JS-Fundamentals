6dca09f8de002565bf84c55eeeb45d4d3b7d2b3a
BenTachie <tachiebenedict@gmail.com>
2025-07-09 01:27:56 +0200

    feat: print multiple lines using an array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+//This script prints three different strings, each on a new line, using a loop.
+const lang = ['C is fun','Python is cool','JavaScript is amazing'];
+for (let i = 0; i < lang.length; i++) {
+  console.log(lang[i]);
+}
