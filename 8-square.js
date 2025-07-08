768b5eec374f6109fc1a3b269a5abe4f42b89035
BenTachie <tachiebenedict@gmail.com>
2025-07-09 01:57:45 +0200

    feat: add script to print a square of size N using the character 'X'

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+// This script prints a square of size N using the character 'X'
+
+const size = Number(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
