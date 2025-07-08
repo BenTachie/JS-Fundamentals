71784923e81b647bab238a6243a11902c5493c9d
BenTachie <tachiebenedict@gmail.com>
2025-07-09 00:59:02 +0200

    feat: add script to concatenate two CLI arguments in '<arg1> is <arg2>' format

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+//This script concatenates two strings passed as command line arguments
+// and prints the result.
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(`${arg1} is ${arg2}`);
